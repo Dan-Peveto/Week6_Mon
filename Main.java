b80933ac54562f1a3e03b0eab2903dcf57d6a447
dan-peveto <daniel_peveto@outlook.com>
2023-11-27 13:25:42 -0800

    added TestJFrame class

---
+++ Main.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("GUI Test");
+        window.setVisible(true);
+        window.setLocation(900, 0);
+        window.setSize(500, 600);
+        window.setVisible(true);
+        TestJFrame window2 = new TestJFrame();
+    }
+
+    
+    
+}
---
+++ TestJFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class TestJFrame extends JFrame {
+    public TestJFrame() {
+        super("Test JFrame");
+        setLocation(0, 900);
+        setSize(500, 600);
+        setVisible(true);
+
+    }
+}
